678877ab9c8e2d55f897f7df0daa4773b5a7f86f
haxxpet <haxxpet@gmail.com>
2020-11-02 19:15:52 +0100

    First commit of function for hashing files

---
+++ function_hashme.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#***********************************#
+# hashme.sh					 		#
+# author: michael					#
+# oct 29, 2020						#
+#									#
+# Hash files for integrity check	#
+#***********************************#
+
+# Declare error codes
+E_FILENOTFOUND=1
+
+# Declare file to hash, used for testing purposes
+#+ this will later be passed to the function by the backup script
+file='/home/hxp/Documents/Skola/one.log'
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++#
+# hash_me ()										#
+# Parameter: target	file name						#
+# returns 0 on success, 1 if file doesn't exists 	#
+#+++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+function hashme () {
+	FILENAME=$1 # Pass argument 1 to FILENAME
+	if [ -f $FILENAME ] # Check if file exists
+	then
+		file_hash=$(sha256sum $FILENAME)
+		echo $file_hash
+		return 0 # success
+	else # if file doesn't exist, return error 1
+		return $E_FILENOTFOUND
+	fi
+}
+
+# send file to hashme
+hashme $file
