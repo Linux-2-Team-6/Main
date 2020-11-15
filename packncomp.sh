7d381d3d7fdb6a88b7316b2fdfe66377cf423a3f
Mikko <mikkohellden1@gmail.com>
2020-11-15 16:10:55 +0100

    Final edition

--- packncomp.sh
+++ packncomp.sh
@@ -5,23 +5,27 @@
 packnpress () {
 
 	THETARGET+=($@) # The chosen files
-	X_SUCCESS=0
-	X_FAIL=1
-
+	PACK_SUCCESS=0
+	PACK_FAIL=1
+	
 	for file in "${THETARGET[@]}"
 	do
 	if [[ ! -f "$file" ]] && [[ ! -d "$file" ]] # This part checks if it is the correct file format.	
 	then	
-		echo "Not a valid source"
-		exit
-	
+		echo "$file is not a valid source"
+		PACK_STATUS=$PACK_FAIL	
+	else 
+	 PACK_STATUS=$PACK_SUCCESS
 
-#echo $@ This is only for testing purposes.
-
-		else
-		tar --backup -cvzf backups.tar.gz "$file" # Here we create the zip and compress it.
-	fi	
+	fi
 	done
+	
+echo "${THETARGET[@]}" This is only for testing purposes.
+
+	if [[ $PACK_STATUS -eq "0" ]]
+	then	
+		tar --backup -cvzf backups.tar.gz "${THETARGET[@]}" # Here we create the zip and compress it.
+	fi
 }
 
 packnpress $@
